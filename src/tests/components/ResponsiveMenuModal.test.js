f5c63bab1566416ae947d61504e29cdbc9b76a6d
Can Ural <can.ural@outlook.com>
2021-01-08 22:41:55 +0200

    Wrote tests for ResponsiveNav and ResponsiveMenuModal components

---
+++ src/tests/components/ResponsiveMenuModal.test.js
@@ -0,0 +1,59 @@
+import { mount } from "enzyme";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import * as ReactReduxHooks from "utils/react-redux-hooks";
+import { mockStore } from "tests/store";
+
+import {
+  CHANGE_HEADER,
+  CLEAR_ERROR,
+  CLOSE_RESPONSIVE_MENU,
+} from "redux/actions/news";
+
+import ResponsiveMenuModal from "components/responsive-menu-modal/ResponsiveMenuModal";
+
+describe("Testing ResponsiveMenuModal component", () => {
+  let wrapper;
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      responsiveMenu: true,
+    });
+
+    jest
+      .spyOn(ReactReduxHooks, "useDispatch")
+      .mockImplementation(() => store.dispatch);
+
+    wrapper = mount(
+      <Router>
+        <ResponsiveMenuModal store={store} />
+      </Router>
+    );
+  });
+  test("Should render the component", () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  test("Should match the snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  test("Should close responsive menu on click container click", () => {
+    wrapper.find("div").first().simulate("click");
+    const actions = store.getActions();
+
+    expect(actions).toEqual([{ type: CLOSE_RESPONSIVE_MENU }]);
+  });
+
+  test("Should close responsive menu on menu click", () => {
+    wrapper.find("span").first().simulate("click");
+    const actions = store.getActions();
+
+    expect(actions).toEqual([
+      { type: CHANGE_HEADER, payload: "home" },
+      { type: CLOSE_RESPONSIVE_MENU },
+      { type: CLEAR_ERROR },
+      { type: CLOSE_RESPONSIVE_MENU },
+    ]);
+  });
+});
---
+++ src/tests/components/ResponsiveNav.test.js
@@ -0,0 +1,75 @@
+import { mount, shallow } from "enzyme";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import * as ReactReduxHooks from "utils/react-redux-hooks";
+import { mockStore } from "tests/store";
+
+import ResponsiveNav from "components/responsive-nav/ResponsiveNav";
+import { user } from "tests/fixtures/user";
+import Search from "components/search/Search";
+import StoryTopicHeaders from "components/story-topic-headers/StoryTopicHeaders";
+
+describe("Testing ResponsiveNav component", () => {
+  let wrapper;
+  let store;
+
+  beforeEach(() => {
+    jest
+      .spyOn(ReactReduxHooks, "useSelector")
+      .mockImplementation((state) => store.getState());
+
+    jest
+      .spyOn(ReactReduxHooks, "useDispatch")
+      .mockImplementation(() => store.dispatch);
+  });
+
+  describe("Testing for when the width is smaller than 768", () => {
+    beforeEach(() => {
+      store = mockStore({
+        width: 700,
+        user,
+      });
+
+      wrapper = mount(
+        <Router>
+          <ResponsiveNav store={store} />
+        </Router>
+      );
+    });
+
+    test("Should render the component with Nav and Search", () => {
+      expect(wrapper).toHaveLength(1);
+
+      expect(wrapper.containsMatchingElement(<Search />)).toBeTruthy();
+    });
+    test("Should match the snapshot", () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe("Testing the component for when the width is bigger than 768", () => {
+    beforeEach(() => {
+      store = mockStore({
+        user,
+        width: 900,
+      });
+
+      wrapper = mount(
+        <Router>
+          <ResponsiveNav store={store} />
+        </Router>
+      );
+    });
+
+    test("Should render the component with Nav and StoryTopicHeaders", () => {
+      expect(wrapper).toHaveLength(1);
+
+      expect(
+        wrapper.containsMatchingElement(<StoryTopicHeaders />)
+      ).toBeTruthy();
+    });
+    test("Should match the snapshot", () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});

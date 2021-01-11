8f18721d84d574be10c164f8f68f74fc5bc74c28
Can Ural <can.ural@outlook.com>
2021-01-11 23:49:09 +0200

    Added tests for UserAvatar and UserDetails components

---
+++ src/tests/components/UserAvatar.test.js
@@ -0,0 +1,64 @@
+import { shallow } from "enzyme";
+import moxios from "moxios";
+
+import * as ReactReduxHooks from "utils/react-redux-hooks";
+import { mockStore } from "tests/store";
+
+import { user, userInputLogin } from "tests/fixtures/user";
+import { fetchUploadAvatar, UPLOAD_AVATAR } from "redux/actions/user";
+
+import UserAvatar from "components/user-avatar/UserAvatar";
+
+describe("Testing UserAvatar component", () => {
+  let wrapper;
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({});
+
+    jest
+      .spyOn(ReactReduxHooks, "useDispatch")
+      .mockImplementation(() => store.dispatch);
+
+    wrapper = shallow(<UserAvatar store={store} />);
+
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  test("Should render the component", () => {
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.find("label").text()).toEqual("Change Avatar*");
+  });
+  test("Should match the snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  test("Should dispatch the upload avatar action on change", async () => {
+    // target here has an input on file upload so I'm simulating that
+    wrapper
+      .find("input")
+      .simulate("change", { target: { files: userInputLogin } });
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: user,
+      });
+    });
+
+    await store.dispatch(fetchUploadAvatar(userInputLogin));
+
+    const actions = store.getActions();
+    // fetchUploadAvatar action takes input and returns user with avatar
+    expect(actions).toEqual([
+      {
+        type: UPLOAD_AVATAR,
+        payload: user,
+      },
+    ]);
+  });
+});
---
+++ src/tests/components/UserDetails.test.js
@@ -0,0 +1,84 @@
+import { shallow } from "enzyme";
+
+import * as ReactReduxHooks from "utils/react-redux-hooks";
+import { mockStore } from "tests/store";
+import { user } from "tests/fixtures/user";
+import UserDetails from "components/user-details/UserDetails";
+import UserAvatar from "components/user-avatar/UserAvatar";
+import UserUpdate from "components/user-update/UserUpdate";
+import DeleteModal from "components/delete-modal/DeleteModal";
+
+describe("Testing UserDetails component", () => {
+  let wrapper;
+  let store;
+
+  beforeEach(() => {
+    jest
+      .spyOn(ReactReduxHooks, "useSelector")
+      .mockImplementation((state) => store.getState());
+  });
+
+  describe("Testing the component with user", () => {
+    beforeEach(() => {
+      store = mockStore({
+        user,
+        deleteModal: false,
+        width: 1100,
+      });
+
+      wrapper = shallow(<UserDetails store={store} />);
+    });
+
+    test("Should render the component when width is smaller than 1150", () => {
+      expect(wrapper).toHaveLength(1);
+      expect(wrapper.containsMatchingElement(<UserUpdate />)).toBeTruthy();
+    });
+    test("Should match snapshot", () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+    test("Should render the component when width is bigger than 1150", () => {
+      store = mockStore({
+        width: 1200,
+        user,
+        deleteModal: false,
+      });
+
+      wrapper = shallow(<UserDetails store={store} />);
+      expect(wrapper).toHaveLength(1);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+  describe("Testing component without user", () => {
+    beforeEach(() => {
+      store = mockStore({
+        user: {},
+        width: 1200,
+        deleteModal: false,
+      });
+
+      wrapper = shallow(<UserDetails store={store} />);
+    });
+
+    test("Should render component without user", () => {
+      expect(wrapper).toHaveLength(1);
+      expect(wrapper.containsMatchingElement(<UserAvatar />)).toBeFalsy();
+      expect(wrapper.containsMatchingElement(<DeleteModal />)).toBeFalsy();
+    });
+    test("Should match snapshot", () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+    test("Should show delete modal", () => {
+      store = mockStore({
+        user,
+        deleteModal: true,
+        width: 1100,
+      });
+
+      wrapper = shallow(<UserDetails />);
+
+      expect(wrapper).toHaveLength(1);
+      expect(wrapper.containsMatchingElement(<DeleteModal />)).toBeTruthy();
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
